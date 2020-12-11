f76347109aedabdff43f4a386701d35206b0b491
Lucas Russo <lerwys@gmail.com>
2020-12-11 19:06:29 -0300

    top: add new top design with RTM SFP

---
+++ hdl/top/afc_rtm_sfp_design/Manifest.py
@@ -0,0 +1,17 @@
+files = [
+    "afc_rtm_sfp_fofb_ctrl.vhd",
+]
+
+fetchto = "../../ip_cores"
+
+modules = {
+    "local" : [
+        "../..",
+    ],
+    "git" : [
+        "https://github.com/lnls-dig/infra-cores.git",
+        "https://github.com/lnls-dig/general-cores.git",
+        "https://github.com/lnls-dig/afc-gw.git",
+        "https://github.com/lnls-dig/CommsCtrlFPGA.git",
+    ],
+}
