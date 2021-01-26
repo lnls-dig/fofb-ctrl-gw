7cdc0b2ed5b707e4fc22b6d66db1bff1268f62ac
Lucas Russo <lerwys@gmail.com>
2021-01-26 09:59:15 -0300

    fofb_ctrl_wrapper: fix cheby RAM generation by spliting the reg space
    RAM generation address decoding only seems to work
    if the RAM is at the upper address space.
    
    A rising edge is needed (monostable) by the fofb_cc_cfg_if
    to trigger a RAM read. So, we use a MONOSTABLE register field
    to generate a 1-CC pulse.

--- hdl/modules/fofb_ctrl_wrapper/cheby/wb_fofb_cc_csr.h
+++ hdl/modules/fofb_ctrl_wrapper/cheby/wb_fofb_cc_csr.h
@@ -1,26 +1,36 @@
 #ifndef __CHEBY__FOFB_CC_CSR__H__
 #define __CHEBY__FOFB_CC_CSR__H__
-#define FOFB_CC_CSR_SIZE 8196 /* 0x2004 */
+#define FOFB_CC_CSR_SIZE 16384 /* 0x4000 = 16KB */
+
+/* FOFB CC configuration register */
+#define FOFB_CC_CSR_CFG_VAL 0x0UL
+
+/* FOFB CC control register */
+#define FOFB_CC_CSR_CFG_CTL 0x4UL
+#define FOFB_CC_CSR_CFG_CTL_READ_RAM 0x1UL
 
 /* FOFB CC RAM for register map */
-#define FOFB_CC_CSR_RAM_REG 0x0UL
+#define FOFB_CC_CSR_RAM_REG 0x2000UL
 #define FOFB_CC_CSR_RAM_REG_SIZE 4 /* 0x4 */
 
 /* None */
 #define FOFB_CC_CSR_RAM_REG_DATA 0x0UL
 
-/* FOFB CC configuration register */
-#define FOFB_CC_CSR_CFG_VAL 0x2000UL
-
 struct fofb_cc_csr {
-  /* [0x0]: MEMORY FOFB CC RAM for register map */
+  /* [0x0]: REG (rw) FOFB CC configuration register */
+  uint32_t cfg_val;
+
+  /* [0x4]: REG (rw) FOFB CC control register */
+  uint32_t cfg_ctl;
+
+  /* padding to: 2048 words */
+  uint32_t __padding_0[2046];
+
+  /* [0x2000]: MEMORY FOFB CC RAM for register map */
   struct ram_reg {
     /* [0x0]: REG (rw) (no description) */
     uint32_t data;
   } ram_reg[2048];
-
-  /* [0x2000]: REG (rw) FOFB CC configuration register */
-  uint32_t cfg_val;
 };
 
 #endif /* __CHEBY__FOFB_CC_CSR__H__ */
