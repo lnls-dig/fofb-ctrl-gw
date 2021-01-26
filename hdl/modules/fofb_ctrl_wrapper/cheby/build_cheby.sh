127409d002b9901f97816be58b3c87f55620f520
Lucas Russo <lerwys@gmail.com>
2021-01-26 15:38:08 -0300

    modules/fofb_ctrl_wrapper: rename "fofb_cc_csr" to "fofb_cc_regs" for convention

--- hdl/modules/fofb_ctrl_wrapper/cheby/build_cheby.sh
+++ hdl/modules/fofb_ctrl_wrapper/cheby/build_cheby.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cheby -i fofb_cc_csr.cheby --hdl vhdl --gen-wbgen-hdl wb_fofb_cc_csr.vhd --doc html --gen-doc doc/wb_fofb_cc_csr_wb.html --gen-c wb_fofb_cc_csr.h --consts-style verilog --gen-consts ../../../sim/regs/wb_fofb_cc_csr.vh
+cheby -i fofb_cc_regs.cheby --hdl vhdl --gen-wbgen-hdl wb_fofb_cc_regs.vhd --doc html --gen-doc doc/wb_fofb_cc_regs_wb.html --gen-c wb_fofb_cc_regs.h --consts-style verilog --gen-consts ../../../sim/regs/wb_fofb_cc_regs.vh
