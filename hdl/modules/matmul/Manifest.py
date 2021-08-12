8651b58d6967533e431d19e0b2817630a06338d3
Melissa Aguiar <melissa.aguiar@engenharia.ufjf.br>
2021-08-12 16:28:19 -0300

    matmul/cheby: organizing files

--- hdl/modules/matmul/Manifest.py
+++ hdl/modules/matmul/Manifest.py
@@ -1,4 +1,4 @@
-files = ["fofb_matmul_top.vhd", "mac_fofb.vhd", "matmul.vhd", "mult_pkg.vhd", "matmul.xdc", "test_matmul.vhd"];
+files = ["fofb_matmul_top.vhd", "mac_fofb.vhd", "matmul.vhd", "mult_pkg.vhd", "matmul.xdc", "test_matmul.vhd", "cheby/matmul_wb.vhd"];
 
 modules = {
   "local" : [ "../../ip_cores/general-cores"],
