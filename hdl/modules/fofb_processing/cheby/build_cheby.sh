825dbc4ecaf02859e498153cf21a3d64c43f2ee7
Guilherme Ricioli <guilherme.riciolic@gmail.com>
2022-09-15 15:13:53 -0300

    Use vhdl pkg of wishbone consts generated by cheby

--- hdl/modules/fofb_processing/cheby/build_cheby.sh
+++ hdl/modules/fofb_processing/cheby/build_cheby.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cheby -i wb_fofb_processing_regs.cheby --hdl vhdl --gen-wbgen-hdl wb_fofb_processing_regs.vhd --doc html --gen-doc doc/wb_fofb_processing_regs.html --gen-c wb_fofb_processing_regs.h --consts-style verilog --gen-consts ../../../sim/regs/wb_fofb_processing_regs.vh
+cheby -i wb_fofb_processing_regs.cheby --hdl vhdl --gen-wbgen-hdl wb_fofb_processing_regs.vhd --doc html --gen-doc doc/wb_fofb_processing_regs.html --gen-c wb_fofb_processing_regs.h --consts-style verilog --gen-consts ../../../sim/regs/wb_fofb_processing_regs.vh --consts-style vhdl-ohwr --gen-consts ../../../sim/regs/wb_fofb_processing_regs_consts_pkg.vhd
