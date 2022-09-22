010c491f0f0a208d503398813f95bb8dded5ac6f
Guilherme Ricioli <guilherme.riciolic@gmail.com>
2022-09-22 13:53:09 -0300

    Add '--assert-level=error" flag
    Assertions of severity level 'error' stop the simulation returning 1 as
    its exit status. This modification is part of an effort to ease future
    CI implantation.

--- hdl/testbench/xwb_fofb_processing/ghdl/Manifest.py
+++ hdl/testbench/xwb_fofb_processing/ghdl/Manifest.py
@@ -8,4 +8,4 @@ modules = {"local" : ["../"]}
 
 ghdl_opt = "--std=08 -frelaxed"
 
-sim_post_cmd = "ghdl -r --std=08 %s --wave=%s.ghw"% (top_module, top_module)
+sim_post_cmd = "ghdl -r --std=08 %s --wave=%s.ghw --assert-level=error"% (top_module, top_module)
