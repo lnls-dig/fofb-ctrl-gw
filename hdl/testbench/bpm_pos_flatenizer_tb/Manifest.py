59996ffec74b227339f9b76a12d6bc4fc5347555
guilherme.ricioli <guilherme.riciolic@gmail.com>
2023-03-31 13:20:19 -0300

    testbench/bpm_pos_flatenizer_tb: add testbench

---
+++ hdl/testbench/bpm_pos_flatenizer_tb/Manifest.py
@@ -0,0 +1,12 @@
+files = [
+    "bpm_pos_flatenizer_tb.vhd",
+    "../fofb_tb_pkg.vhd",
+]
+
+modules = {
+    "local" : [
+        "../../ip_cores/general-cores",
+        "../../ip_cores/CommsCtrlFPGA",
+        "../../modules",
+    ],
+}
