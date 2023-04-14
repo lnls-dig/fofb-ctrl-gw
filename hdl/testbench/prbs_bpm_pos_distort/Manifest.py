3adfad00fba3977bd50afdbcbd2351f15df06197
guilherme.ricioli <guilherme.riciolic@gmail.com>
2023-04-14 16:48:28 -0300

    testbench/prbs_bpm_pos_distort: add testbench

---
+++ hdl/testbench/prbs_bpm_pos_distort/Manifest.py
@@ -0,0 +1,11 @@
+files = [
+    "prbs_bpm_pos_distort_tb.vhd",
+    "../fofb_tb_pkg.vhd"
+]
+
+modules = {
+    "local" : [
+        "../../ip_cores/infra-cores",
+        "../../modules"
+    ],
+}
