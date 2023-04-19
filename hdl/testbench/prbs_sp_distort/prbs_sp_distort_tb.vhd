261cec2a2df5d9aef1b13788e2419f1a5ab91422
guilherme.ricioli <guilherme.riciolic@gmail.com>
2023-04-19 11:55:32 -0300

    prbs_sp_distort.vhd: wrap prbs_gen_for_sys_id
    Wrap PRBS generator.

--- hdl/testbench/prbs_sp_distort/prbs_sp_distort_tb.vhd
+++ hdl/testbench/prbs_sp_distort/prbs_sp_distort_tb.vhd
@@ -6,7 +6,7 @@
 -- Platform   : Simulation
 -- Standard   : VHDL 2008
 --------------------------------------------------------------------------------
--- Description: Tests chaining prbs_gen_for_sys_id with prbs_sp_distort.
+-- Description: Tests prbs_gen_for_sys_id.
 --------------------------------------------------------------------------------
 -- Copyright (c) 2023 CNPEM
 -- Licensed under GNU Lesser General Public License (LGPL) v3.0
@@ -22,7 +22,6 @@ use ieee.numeric_std.all;
 
 library work;
 use work.fofb_tb_pkg.all;
-use work.ifc_common_pkg.all;
 use work.fofb_sys_id_pkg.all;
 
 entity prbs_sp_distort_tb is
@@ -38,13 +37,16 @@ architecture test of prbs_sp_distort_tb is
 
   signal clk                          : std_logic := '0';
   signal rst_n                        : std_logic := '0';
-  signal prbs                         : std_logic := '0';
-  signal prbs_gen_for_sys_id_valid_i  : std_logic := '0';
-  signal prbs_gen_for_sys_id_valid_o  : std_logic := '0';
+  signal en_distort                   : std_logic := '1';
+  signal prbs_rst_n                   : std_logic := '1';
+  signal prbs_step_duration           : natural range 1 to 1024 := 1;
+  signal prbs_lfsr_length             : natural range 2 to 32 := 32;
+  signal prbs_valid                   : std_logic;
   signal sp                           : signed(c_SP_WIDTH-1 downto 0);
   signal sp_valid                     : std_logic := '0';
   signal distort_sp                   : signed(c_SP_WIDTH-1 downto 0);
   signal distort_sp_valid             : std_logic := '0';
+  signal prbs                         : std_logic := '0';
 begin
   f_gen_clk(100_000_000, clk);
 
@@ -53,18 +55,25 @@ begin
       signed(maximum(c_SP_WIDTH, c_DISTORT_LEVEL_WIDTH) downto 0);
     variable v_expec_distort_sp : signed(c_SP_WIDTH-1 downto 0);
   begin
+    prbs_step_duration <= 2;
+    prbs_lfsr_length <= 7;
+
     rst_n <= '0';
     f_wait_cycles(clk, 5);
     rst_n <= '1';
     f_wait_cycles(clk, 5);
 
+    en_distort <= '1';
+    f_wait_cycles(clk, 5);
+
+    -- PRBS-based distortion enabled
     for sp_val in -(2**(c_SP_WIDTH-1)) to 2**(c_SP_WIDTH-1)-1
     loop
       -- Iterate PRBS
-      prbs_gen_for_sys_id_valid_i <= '1';
+      prbs_valid <= '1';
       f_wait_cycles(clk, 1);
-      prbs_gen_for_sys_id_valid_i <= '0';
-      f_wait_clocked_signal(clk, prbs_gen_for_sys_id_valid_o, '1');
+      prbs_valid <= '0';
+      f_wait_cycles(clk, 1);
 
       -- Drive setpoint
       sp <= to_signed(sp_val, sp'length);
@@ -95,21 +104,32 @@ begin
         severity error;
     end loop;
 
+    en_distort <= '0';
+    f_wait_cycles(clk, 5);
+
+    -- PRBS-based distortion disabled
+    for sp_val in -(2**(c_SP_WIDTH-1)) to 2**(c_SP_WIDTH-1)-1
+    loop
+      -- Drive setpoint
+      sp <= to_signed(sp_val, sp'length);
+      sp_valid <= '1';
+      f_wait_cycles(clk, 1);
+      sp_valid <= '0';
+      f_wait_clocked_signal(clk, distort_sp_valid, '1');
+
+      v_expec_distort_sp := to_signed(sp_val, distort_sp'length);
+
+      -- Checks sepoints
+      assert distort_sp = v_expec_distort_sp
+        report
+          "Wrong sepoints: " &
+          integer'image(to_integer(distort_sp)) & " (expected " &
+          integer'image(to_integer(v_expec_distort_sp)) & ")"
+        severity error;
+    end loop;
+
     std.env.finish;
   end process;
-
-  cmp_prbs_gen_for_sys_id : prbs_gen_for_sys_id
-    port map (
-      clk_i           => clk,
-      rst_n_i         => rst_n,
-      en_i            => '1',
-      step_duration_i => 1,
-      lfsr_length_i   => 7,
-      valid_i         => prbs_gen_for_sys_id_valid_i,
-      busy_o          => open,
-      prbs_o          => prbs,
-      valid_o         => prbs_gen_for_sys_id_valid_o
-    );
 
   uut : prbs_sp_distort
     generic map (
@@ -119,13 +139,18 @@ begin
     port map (
       clk_i                 => clk,
       rst_n_i               => rst_n,
+      en_distort_i          => en_distort,
+      prbs_rst_n_i          => prbs_rst_n,
+      prbs_step_duration_i  => prbs_step_duration,
+      prbs_lfsr_length_i    => prbs_lfsr_length,
+      prbs_valid_i          => prbs_valid,
       sp_i                  => sp,
       sp_valid_i            => sp_valid,
-      prbs_i                => prbs,
       distort_level_0_i     => c_DISTORT_LEVEL_0,
       distort_level_1_i     => c_DISTORT_LEVEL_1,
       distort_sp_o          => distort_sp,
-      distort_sp_valid_o    => distort_sp_valid
+      distort_sp_valid_o    => distort_sp_valid,
+      prbs_o                => prbs
     );
 
 end architecture test;
