2f2625080152118093a3b8834ffad6329a852ef4
Augusto Fraga Giachero <afg@augustofg.net>
2024-05-07 16:44:02 -0300

    Remove support for AFCv3 boards
    These synthesis targets are not updated since a long time, we don't
    use them anymore. Remove them.

--- hdl/top/afcv3_ref_design/Manifest.py
+++
@@ -1,9 +0,0 @@
-files = [
-    "afcv3_ref_fofb_ctrl.vhd",
-]
-
-modules = {
-    "local" : [
-        "../afc_ref_design_gen",
-    ],
-}
--- hdl/top/afcv3_rtm_sfp_design/Manifest.py
+++
@@ -1,9 +0,0 @@
-files = [
-    "afcv3_rtm_sfp_fofb_ctrl.vhd",
-]
-
-modules = {
-    "local" : [
-        "../afc_ref_design_gen",
-    ],
-}
